4391bb3a28c99840653f149be15e63957cb96947
Abhishek Shukla <abhishekshukla8097@gmail.com>
2025-12-11 17:54:03 +0530

    Register page

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react'
 import {
     Card,
@@ -10,48 +12,74 @@ import {
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
 import { Label } from '@/components/ui/label'
+import { useState } from 'react'
+import { redirect } from 'next/navigation'
 
-const page = () => {
+const Register = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
     return (
-            <Card className=" max-w-sm mx-auto mt-30 ">
-                <CardHeader>
-                    <CardTitle className="text-2xl">Register Here</CardTitle>
-                    <CardAction>
-                        <Button variant="link">Sign In</Button>
-                    </CardAction>
-                </CardHeader>
-                <CardContent>
-                    <form>
-                        <div className="flex flex-col gap-6">
-                            <div className="grid gap-2">
-                                <Label htmlFor="email">Email</Label>
-                                <Input
-                                    id="email"
-                                    type="email"
-                                    placeholder="m@example.com"
-                                    required
-                                />
-                            </div>
-                            <div className="grid gap-2">
-                                <div className="flex items-center">
-                                    <Label htmlFor="password">Password</Label>
-                                </div>
-                                <Input id="password" type="password" placeholder='**********' required />
-                            </div>
+        <Card className=" max-w-sm mx-auto mt-30 ">
+            <CardHeader>
+                <CardTitle className="text-2xl">Register Here</CardTitle>
+                <CardAction>
+                    <Button variant="link" className="cursor-pointer" onClick={() => { redirect('/login') }}>Sign In</Button>
+                </CardAction>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div className="flex flex-col gap-6">
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Username</Label>
+                            <Input
+                                id="username"
+                                type="username"
+                                placeholder="m@example.com"
+                                required
+                                value={username}
+                                onChange={(e) => setUsername(e.target.value)}
+                            />
                         </div>
-                    </form>
-                </CardContent>
-                <CardFooter className="flex-col gap-2">
-                    <Button type="submit" className="w-full">
-                        Login
-                    </Button>
-
-                    <Button variant="outline" className="w-full">
-                      <i class="fa-brands fa-google text-[20px]"></i>  Continue with Google
-                    </Button>
-                </CardFooter>
-            </Card>
+                        <div className="grid gap-2">
+                            <div className="flex items-center">
+                                <Label htmlFor="password">Password</Label>
+                            </div>
+                            <Input id="password"
+                                type="password"
+                                placeholder='**********'
+                                required
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <div className="grid gap-2">
+                            <Label htmlFor="email">Email</Label>
+                            <Input
+                                id="email"
+                                type="email"
+                                placeholder="m@example.com"
+                                required
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex-col gap-2">
+                <Button type="submit" className="w-full">
+                    Login
+                </Button>
+                <Button variant="outline" className="w-full cursor-pointer">
+                    <i className="fa-brands fa-google text-[20px]"></i>  Continue with Google
+                </Button>
+                <Button variant="outline" className="w-full cursor-pointer" onClick={() => signIn('google')}>
+                    <i className="fa-brands fa-github text-[20px] "></i>  Continue with GitHub
+                </Button>
+            </CardFooter>
+        </Card>
     )
 }
 
-export default page
+export default Register
