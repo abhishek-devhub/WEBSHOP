da9a6af02e24b0ec1babe9d5a924d294ae355342
Abhishek Shukla <abhishekshukla8097@gmail.com>
2025-12-17 19:32:52 +0530

    updated addtocart component with toast and size functionality

--- components/AddtoCart.js
+++ components/AddtoCart.js
@@ -1,17 +1,28 @@
 'use client'
 
 import React from 'react'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { addItem } from '@/app/redux/addcart/cartslice'
+import { toast } from 'react-toastify'
 
 const AddtoCart = ({ product }) => {
     const dispatch = useDispatch()
+    const SelectedSize = useSelector(state => state.size.SelectedSize)
+
+    function handleCart() {
+        if (!SelectedSize) {
+            toast.error('please select the size')
+            return
+        }
+        dispatch(addItem({ ...product, size: SelectedSize }))
+    }
     return (
         <div>
-            <button className="bg-orange-500 hover:bg-orange-600 text-white px-8 py-3 rounded-lg font-semibold cursor-pointer" onClick={() =>  dispatch(addItem(product))}>
+            <button className="bg-orange-500 hover:bg-orange-600 text-white px-8 py-3 rounded-lg font-semibold cursor-pointer" onClick={() => handleCart()}>
                 Add to Cart
             </button>
         </div>
+
     )
 }
 
