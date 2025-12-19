6b37d3715e1251818734989005127b23cfdfa3c7
Abhishek Shukla <abhishekshukla8097@gmail.com>
2025-12-19 14:26:54 +0530

    Enhance Navbar component with cart count and login condition

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,26 +1,29 @@
+'use client'
 import React from "react";
 import Link from "next/link";
+import { useSelector } from 'react-redux'
+import { Selectcartcount } from "@/app/redux/addcart/cartslice";
+import Login from "@/app/(auth)/login/page";
 
 const Navbar = () => {
+  const cartCount = useSelector(Selectcartcount)
   return (
     <header className="w-full bg-white border-b">
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
 
-        {/* Logo */}
+
         <Link href="/" className="flex items-center gap-2">
           <i className="fa-brands fa-wolf-pack-battalion text-3xl text-orange-500"></i>
           <span className="text-2xl font-bold tracking-wide">WolfStore</span>
         </Link>
 
-        {/* Center Menu */}
         <nav className="hidden md:flex items-center gap-8 text-gray-800 font-semibold tracking-wide">
           <Link href="/Men" className="hover:text-orange-600 transition">Men</Link>
-          <Link href="#" className="hover:text-orange-600 transition">Women</Link>
-          <Link href="#" className="hover:text-orange-600 transition">Kids</Link>
-          <Link href="#" className="hover:text-orange-600 transition">New Arrivals</Link>
+          <Link href="/women" className="hover:text-orange-600 transition">Women</Link>
+          <Link href="/kids" className="hover:text-orange-600 transition">Kids</Link>
+          <Link href="/newarrivials" className="hover:text-orange-600 transition">New Arrivals</Link>
         </nav>
 
-        {/* Right Icons */}
         <div className="flex items-center gap-6 text-gray-700">
           
           <Link href="/search" className="hover:text-orange-600 transition text-xl">
@@ -29,17 +32,17 @@ const Navbar = () => {
 
           <Link href="/cart" className="hover:text-orange-600 transition text-xl relative">
             <i className="fa-solid fa-cart-shopping"></i>
-            <span className="absolute -top-2 -right-3 bg-orange-600 text-white text-xs px-1.5 py-0.5 rounded-full">
-              3
+            <span className="absolute -top-2 -right-3 bg-orange-600 text-white text-xs px-1.5 py-0.5 rounded-full" >
+             {cartCount}
             </span>
           </Link>
 
-          <Link 
+          {!Login && <Link 
             href="/login" 
             className="px-4 py-2 rounded-md bg-orange-400 text-white font-medium hover:bg-orange-500 transition"
           >
             Login
-          </Link>
+          </Link>}
         </div>
       </div>
     </header>
