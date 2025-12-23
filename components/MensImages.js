4e2ca6fa28e6e25e3b4a36d341ec2e55bab5f815
Abhishek Shukla <abhishekshukla8097@gmail.com>
2025-12-23 20:39:52 +0530

    sizeslector through id and some common change

--- components/MensImages.js
+++ components/MensImages.js
@@ -1,10 +1,4 @@
-import cloudinary from "@/lib/Cloudinary";
-
 export const getMensImages = async () => {
-  // const result = await cloudinary.api.resources({
-  //     type: "upload",
-  //     resource_type: "image",
-  //   });
 
       const res = await fetch('http://localhost:3000/api/products' ,{
         method:'GET'
--- components/SizeSelctor.js
+++ components/SizeSelctor.js
@@ -4,22 +4,20 @@ import React from 'react'
 import { useSelector , useDispatch } from 'react-redux'
 import { SetSize } from '@/app/redux/Sizeselect/sizeslice'
 
-const SizeSelctor = () => {
- 
+const SizeSelctor = ({product}) => {
+  
   const dispatch = useDispatch()
    const SelectedSize = useSelector(state => state.size.SelectedSize)
-
-  const Sizes = ["S", "M", "L", "XL", "XXL"]
+   
   return (
     <div>
       <div className="flex gap-3">
-                {Sizes.map(Size => (
+                {product.sizes.map(Size => (
                   <button key={Size} className={`border px-4 py-2 rounded hover:border-black font-medium cursor-pointer  ${SelectedSize === Size ? 'bg-green-500' : ''}`} value={SelectedSize} onClick={() => dispatch(SetSize(Size))}>
                     {Size}
                   </button>
                 ))}
               </div>
-              {<p className='mt-2'>Selected Size : {SelectedSize || 'Select your Size'}</p>}
     </div>
   )
 }
